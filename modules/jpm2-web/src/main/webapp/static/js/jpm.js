3a8d5b3363f4b618327e0d3b3749fc613516d350
Jeronimo Paoletti <jeronimo.paoletti@gmail.com>
2013-08-17 11:40:15 -0300

    Some javascript stuff

---
+++ modules/jpm2-web/src/main/webapp/static/js/jpm.js
@@ -0,0 +1,126 @@
+
+$(document).ready(function() {
+    var ul = $('#sidebar > ul');
+    var ul2 = $('#sidebar li.open ul');
+    // === jPanelMenu === //
+    var jPanel = $.jPanelMenu({
+        menu: '#sidebar',
+        trigger: '#menu-trigger'
+    });
+    // === Resize window related === //
+    $(window).resize(function() {
+        if ($(window).width() > 480 && $(window).width() < 769){
+            ul2.css({'display': 'none'});
+            ul.css({'display': 'block'});
+        }
+
+        if ($(window).width() <= 480){
+            ul.css({'display': 'none'});
+            ul2.css({'display': 'block'});
+            if (!$('html').hasClass('jPanelMenu')) {
+                jPanel.on();
+            }
+
+            if ($(window).scrollTop() > 35) {
+                $('body').addClass('fixed');
+            }
+            $(window).scroll(function() {
+                if ($(window).scrollTop() > 35) {
+                    $('body').addClass('fixed');
+                } else {
+                    $('body').removeClass('fixed');
+                }
+            });
+        } else {
+            jPanel.off();
+        }
+        if ($(window).width() > 768){
+            ul.css({'display': 'block'});
+            ul2.css({'display': 'block'});
+            $('#user-nav > ul').css({width: 'auto', margin: '0'});
+        }
+    });
+
+    if ($(window).width() <= 480){
+        if ($(window).scrollTop() > 35) {
+            $('body').addClass('fixed');
+        }
+        $(window).scroll(function() {
+            if ($(window).scrollTop() > 35) {
+                $('body').addClass('fixed');
+            } else {
+                $('body').removeClass('fixed');
+            }
+        });
+        jPanel.on();
+    }
+
+    if ($(window).width() > 480){
+        ul.css({'display': 'block'});
+        jPanel.off();
+    }
+    if ($(window).width() > 480 && $(window).width() < 769) {
+        ul2.css({'display': 'none'});
+    }
+
+    // === Sidebar navigation === //
+
+    $('li.submenu > a').click(function(e){
+        e.preventDefault();
+        var submenu = $(this).siblings('ul');
+        var li = $(this).parents('li');
+        if ($(window).width() > 480) {
+            var submenus = $('#sidebar li.submenu ul');
+            var submenus_parents = $('#sidebar li.submenu');
+        } else {
+            var submenus = $('#jPanelMenu-menu li.submenu ul');
+            var submenus_parents = $('#jPanelMenu-menu li.submenu');
+        }
+
+        if (li.hasClass('open')) {
+            if (($(window).width() > 768) || ($(window).width() <= 480)) {
+                submenu.slideUp();
+            } else {
+                submenu.fadeOut(250);
+            }
+            li.removeClass('open');
+        } else {
+            if (($(window).width() > 768) || ($(window).width() <= 480)) {
+                submenus.slideUp();
+                submenu.slideDown();
+            } else {
+                submenus.fadeOut(250);
+                submenu.fadeIn(250);
+            }
+            submenus_parents.removeClass('open');
+            li.addClass('open');
+        }
+    });
+
+    // === Tooltips === //
+    $('.tip').tooltip();
+    $('.tip-left').tooltip({placement: 'left'});
+    $('.tip-right').tooltip({placement: 'right'});
+    $('.tip-top').tooltip({placement: 'top'});
+    $('.tip-bottom').tooltip({placement: 'bottom'});
+
+    // === Style switcher === //
+    $('#style-switcher i').click(function() {
+        if ($(this).hasClass('open')) {
+            $(this).parent().animate({right: '-=220'});
+            $(this).removeClass('open');
+        } else {
+            $(this).parent().animate({right: '+=220'});
+            $(this).addClass('open');
+        }
+        $(this).toggleClass('glyphicon-arrow-left');
+        $(this).toggleClass('glyphicon-arrow-right');
+    });
+
+    $('#style-switcher a').click(function() {
+        var style = $(this).attr('href').replace('#', '');
+        $('.skin-color').attr('href', 'css/unicorn.' + style + '.css');
+        $(this).siblings('a').css({'border-color': 'transparent'});
+        $(this).css({'border-color': '#aaaaaa'});
+    });
+});
