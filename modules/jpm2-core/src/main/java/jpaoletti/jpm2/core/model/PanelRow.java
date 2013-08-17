bd12381a01709c92759cecb2d14ccf4789b4a41c
Jeronimo Paoletti <jeronimo.paoletti@gmail.com>
2013-08-17 11:27:16 -0300

    panels

---
+++ modules/jpm2-core/src/main/java/jpaoletti/jpm2/core/model/PanelRow.java
@@ -0,0 +1,21 @@
+package jpaoletti.jpm2.core.model;
+
+import java.util.ArrayList;
+
+/**
+ * A panel row.
+ *
+ * @author jpaoletti
+ */
+public class PanelRow {
+
+    private ArrayList<Panel> panels;
+
+    public ArrayList<Panel> getPanels() {
+        return panels;
+    }
+
+    public void setPanels(ArrayList<Panel> panels) {
+        this.panels = panels;
+    }
+}
