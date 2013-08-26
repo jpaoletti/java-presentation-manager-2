9ef0a644a21716c6ab7e1450b344857f6532d25b
Jeronimo Paoletti <jeronimo.paoletti@gmail.com>
2013-08-26 20:34:34 -0300

    added password converter

---
+++ modules/jpm2-web/src/main/java/jpaoletti/jpm2/web/converter/Password.java
@@ -0,0 +1,23 @@
+package jpaoletti.jpm2.web.converter;
+
+import jpaoletti.jpm2.core.converter.Converter;
+import jpaoletti.jpm2.core.converter.ConverterException;
+import jpaoletti.jpm2.core.model.Field;
+import jpaoletti.jpm2.core.security.BCrypt;
+
+/**
+ *
+ * @author jpaoletti
+ */
+public class Password extends Converter {
+
+    @Override
+    public Object visualize(Field field, Object object) throws ConverterException {
+       return "@page:password-converter.jsp";
+    }
+
+    @Override
+    public Object build(Field field, Object newValue) throws ConverterException {
+        return BCrypt.hashpw((String) newValue, BCrypt.gensalt());
+    }
+}
