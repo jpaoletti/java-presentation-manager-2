77d435ddc18dc76d5c5f441106b9f415fe037b0c
Jeronimo Paoletti <jeronimo.paoletti@gmail.com>
2013-09-21 13:12:51 -0300

    enhanced show object converter with a link to an operation

--- modules/jpm2-web/src/main/java/jpaoletti/jpm2/web/converter/ShowObject.java
+++ modules/jpm2-web/src/main/java/jpaoletti/jpm2/web/converter/ShowObject.java
@@ -1,10 +1,14 @@
 package jpaoletti.jpm2.web.converter;
 
+import java.io.Serializable;
 import jpaoletti.jpm2.core.JPMContext;
+import jpaoletti.jpm2.core.NotAuthorizedException;
 import jpaoletti.jpm2.core.converter.Converter;
 import jpaoletti.jpm2.core.converter.ConverterException;
+import jpaoletti.jpm2.core.message.MessageFactory;
 import jpaoletti.jpm2.core.model.Entity;
 import jpaoletti.jpm2.core.model.Field;
+import jpaoletti.jpm2.core.model.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -17,17 +21,50 @@ public class ShowObject extends Converter {
 
     private Entity entity;
     private String fields;
+    private String operation;
     @Autowired
     private JPMContext context;
 
     @Override
     public Object visualize(Field field, Object object, String instanceId) throws ConverterException {
         final Object value = getValue(object, field);
-        final String res = "@page:show-object-converter.jsp?entityId=" + getEntity().getId() + "&fields=" + getFields() + "&objectId=" + getContext().getEntity().getDao().getId(object);
+
+        final String res = "@page:show-object-converter.jsp"
+                + "?entityId=" + getEntity().getId()
+                + "&fields=" + getFields()
+                + "&objectId=" + getContext().getEntity().getDao().getId(object);
         if (value == null) {
             return res;
         } else {
-            return res + "&value=" + value.toString() + "&instanceId=" + getEntity().getDao().getId(value);
+            final Serializable localId = getEntity().getDao().getId(value);
+            String operationLink = "";
+            String operationTitle = "";
+            if (getOperation() != null) {
+                final Operation op = getEntity().getOperation(getOperation());
+                if (op != null) {
+                    try {
+                        op.checkAuthorization();
+                    } catch (NotAuthorizedException ex) {
+                    }
+                    operationTitle = getMessage(op.getTitle(), getMessage(getEntity().getTitle()));
+                    switch (op.getScope()) {
+                        case ITEM:
+                            operationLink = "jpm/" + getEntity().getId() + "/" + localId + "/" + getOperation();
+                            break;
+                        default:
+                            operationLink = "jpm/" + getEntity().getId() + "/" + getOperation();
+                            break;
+                    }
+                } else {
+                    throw new ConverterException(MessageFactory.error("jpm.converter.operation.not.exists", getEntity().getId(), getOperation()));
+                }
+            }
+            return res
+                    + "&value=" + value.toString()
+                    + "&instanceId=" + localId
+                    + "&operationId=" + getOperation()
+                    + "&operationLink=" + operationLink
+                    + "&operationTitle=" + operationTitle;
         }
     }
 
@@ -53,5 +90,13 @@ public class ShowObject extends Converter {
 
     public void setFields(String fields) {
         this.fields = fields;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation;
     }
 }
--- modules/jpm2-web/src/main/webapp/WEB-INF/jsp/converter/show-object-converter.jsp
+++ modules/jpm2-web/src/main/webapp/WEB-INF/jsp/converter/show-object-converter.jsp
@@ -1,4 +1,4 @@
-<c:if test="${not empty value}">
+<c:if test="${not empty param.value}">
     <a href="javascript:;" id="field_${field}_${param.instanceId}_${param.objectId}">${param.value}</a>
     <script type="text/javascript">
         $("body").on("click", "#field_${field}_${param.instanceId}_${param.objectId}", function() {
@@ -7,7 +7,10 @@
                 $.each(data, function(i, v) {
                     content = content + "<tr><th>" + i + "</th><td>" + v + "</td></tr>";
                 });
-                content = content + "</tbody></table><a href=\"javascript:$('" + "#field_${field}_${param.instanceId}_${param.objectId}" + "').popover('hide');\" class='pull-right'>close</a>";
+                content = content + "</tbody></table><a href=\"javascript:$('" + "#field_${field}_${param.instanceId}_${param.objectId}" + "').popover('hide');\" class='pull-right'><i class='glyphicon glyphicon-remove'></i></a>";
+                //<c:if test="${not empty param.operationLink}">
+                content = content + "<a href='${cp}${param.operationLink}'><i class='glyphicon jpmicon-${param.operationId}'></i> ${param.operationTitle}</a>";
+                //</c:if>
                 $("#field_${field}_${param.instanceId}_${param.objectId}").popover({placement: 'top', html: true, content: content}).popover('show');
             });
         });
