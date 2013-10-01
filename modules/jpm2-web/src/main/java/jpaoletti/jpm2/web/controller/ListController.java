4954be8f91393202d12bb38dcf4b9efe1ad2129f
jpaoletti <jpaoletti@pc-yero>
2013-10-01 10:48:49 -0300

    checking field

--- modules/jpm2-web/src/main/java/jpaoletti/jpm2/web/controller/ListController.java
+++ modules/jpm2-web/src/main/java/jpaoletti/jpm2/web/controller/ListController.java
@@ -1,14 +1,13 @@
 package jpaoletti.jpm2.web.controller;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.servlet.http.HttpServletRequest;
 import jpaoletti.jpm2.core.NotAuthorizedException;
 import jpaoletti.jpm2.core.PMException;
+import jpaoletti.jpm2.core.message.MessageFactory;
 import jpaoletti.jpm2.core.model.Entity;
 import jpaoletti.jpm2.core.model.Field;
 import jpaoletti.jpm2.core.model.ListFilter;
@@ -78,6 +77,9 @@ public class ListController extends BaseController {
             while (matcher.find()) {
                 final String _display_field = matcher.group().replaceAll("\\{", "").replaceAll("\\}", "");
                 final Field field2 = entity.getFieldById(_display_field);
+                if (field2 == null) {
+                    throw new PMException(MessageFactory.error("jpm.field.not.found", _display_field));
+                }
                 disjunction.add(Restrictions.like(field2.getProperty(), query, MatchMode.ANYWHERE));
             }
             restrictions.add(disjunction);
