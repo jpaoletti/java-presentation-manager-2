680ce53b21a4d13470f4bfcbea061c95ee59078f
Jeronimo Paoletti <jeronimo.paoletti@gmail.com>
2013-10-18 18:07:15 -0300

    better validation of new user

--- modules/jpm2-core/src/main/java/jpaoletti/jpm2/core/validator/ValidateNewUser.java
+++ modules/jpm2-core/src/main/java/jpaoletti/jpm2/core/validator/ValidateNewUser.java
@@ -1,33 +1,27 @@
 package jpaoletti.jpm2.core.validator;
 
-import jpaoletti.jpm2.core.dao.UserDAO;
-import jpaoletti.jpm2.core.message.Message;
+import jpaoletti.jpm2.core.PMCoreObject;
+import jpaoletti.jpm2.core.exception.EntityNotFoundException;
 import jpaoletti.jpm2.core.message.MessageFactory;
 import jpaoletti.jpm2.core.model.OperationValidator;
 import jpaoletti.jpm2.core.model.ValidationException;
-import jpaoletti.jpm2.core.security.User;
+import org.springframework.security.core.userdetails.UserDetails;
 
 /**
  *
  * @author jpaoletti
  */
-public class ValidateNewUser implements OperationValidator {
-
-    private UserDAO dao;
+public class ValidateNewUser extends PMCoreObject implements OperationValidator {
 
     @Override
     public void validate(Object object) throws ValidationException {
-        final User user = (User) object;
-        if (getDao().get(user.getUsername()) != null) {
-            throw new ValidationException(MessageFactory.error("jpm.validator.userexists", user.getUsername()));
+        try {
+            final UserDetails user = (UserDetails) object;
+            if (getJpm().getEntity("jpm-entity-user").getDao().get(user.getUsername()) != null) {
+                throw new ValidationException(MessageFactory.error("jpm.validator.userexists", user.getUsername()));
+            }
+        } catch (EntityNotFoundException ex) {
+            throw new ValidationException(MessageFactory.error(ex.getMessage()));
         }
-    }
-
-    public UserDAO getDao() {
-        return dao;
-    }
-
-    public void setDao(UserDAO dao) {
-        this.dao = dao;
     }
 }
