e278a9d321db1d459fe4440fe4f0ee62860e3271
Jeronimo Paoletti <jeronimo.paoletti@gmail.com>
2013-10-28 22:43:06 -0300

    added a "properties" property to ToString converter to use a combination of properties instead of "toString"

--- modules/jpm2-core/src/main/java/jpaoletti/jpm2/core/converter/ToStringConverter.java
+++ modules/jpm2-core/src/main/java/jpaoletti/jpm2/core/converter/ToStringConverter.java
@@ -1,22 +1,28 @@
 package jpaoletti.jpm2.core.converter;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import jpaoletti.jpm2.core.exception.ConfigurationException;
 import jpaoletti.jpm2.core.exception.ConverterException;
 import jpaoletti.jpm2.core.model.Field;
 import jpaoletti.jpm2.util.JPMUtils;
 
 /**
+ * properties is something like "{prop1} ... {prop2}" where each prop is a
+ * property of the field value.
  *
  * @author jpaoletti
  */
 public class ToStringConverter extends Converter {
 
+    public static final Pattern DISPLAY_PATTERN = Pattern.compile("\\{.*?\\}");
     private Integer padCount = 0;
     private String prefix;
     private String suffix;
     private Character padChar = ' ';
     private PadDirection padDirection = PadDirection.LEFT;
     private Integer cutOff;
+    private String properties;
 
     @Override
     public Object visualize(Field field, Object object, String instanceId) throws ConverterException, ConfigurationException {
@@ -25,7 +31,7 @@ public class ToStringConverter extends Converter {
     }
 
     protected String process(final Object value) {
-        String res = value != null ? String.valueOf(value) : "";
+        String res = getFinalValue(value, getProperties());
         if (getCutOff() != null && res.length() > getCutOff()) {
             res = res.substring(0, getCutOff()) + "...";
         }
@@ -44,6 +50,26 @@ public class ToStringConverter extends Converter {
             res = res + getSuffix();
         }
         return res;
+    }
+
+    public static String getFinalValue(Object value, String properties) {
+        String finalValue;
+        if (properties != null) {
+            final Matcher matcher = DISPLAY_PATTERN.matcher(properties);
+            finalValue = properties;
+            while (matcher.find()) {
+                final String property = matcher.group();
+                final String _display = property.replaceAll("\\{", "").replaceAll("\\}", "");
+                try {
+                    finalValue = finalValue.replace(property, String.valueOf(JPMUtils.get(value, _display)));
+                } catch (ConfigurationException ex) {
+                    finalValue = finalValue.replace("{" + _display + "}", "?");
+                }
+            }
+        } else {
+            finalValue = String.valueOf(value);
+        }
+        return finalValue;
     }
 
     public Integer getPadCount() {
@@ -92,6 +118,14 @@ public class ToStringConverter extends Converter {
 
     public void setCutOff(Integer cutOff) {
         this.cutOff = cutOff;
+    }
+
+    public String getProperties() {
+        return properties;
+    }
+
+    public void setProperties(String properties) {
+        this.properties = properties;
     }
 
     public static enum PadDirection {
