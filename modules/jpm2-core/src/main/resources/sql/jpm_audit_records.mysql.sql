b38c2cce3b51964ac5cc7d35c3d3b743a572a766
Jeronimo Paoletti <jeronimo.paoletti@gmail.com>
2018-08-13 09:55:20 -0300

    mysql scripts

---
+++ modules/jpm2-core/src/main/resources/sql/jpm_audit_records.mysql.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS `jpm_audit_records` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `datetime` datetime DEFAULT NULL,
+  `entity` varchar(255) DEFAULT NULL,
+  `item` varchar(255) DEFAULT NULL,
+  `observations` varchar(255) DEFAULT NULL,
+  `operation` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
