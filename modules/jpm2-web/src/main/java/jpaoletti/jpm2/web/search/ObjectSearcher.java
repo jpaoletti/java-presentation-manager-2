209998e8266d1832519b01c4c5ff378569df95f9
Jeronimo Jaoletti <jeronimo.paoletti@gmail.com>
2021-01-26 13:01:00 -0300

    New object searcher

---
+++ modules/jpm2-web/src/main/java/jpaoletti/jpm2/web/search/ObjectSearcher.java
@@ -0,0 +1,84 @@
+package jpaoletti.jpm2.web.search;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import jpaoletti.jpm2.core.message.MessageFactory;
+import jpaoletti.jpm2.core.model.Entity;
+import jpaoletti.jpm2.core.model.Field;
+import jpaoletti.jpm2.core.model.ListFilter;
+import jpaoletti.jpm2.core.search.Searcher;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ *
+ * @author jpaoletti
+ */
+public class ObjectSearcher implements Searcher {
+
+    public static final String DESCRIPTION_KEY = "jpm.searcher.objectSearcher";
+
+    private Entity entity;
+    private ListFilter filter;
+    private String textField;
+
+    @Autowired
+    private HttpServletRequest request;
+
+    @Override
+    public String visualization(Entity entity, Field field) {
+        final StringBuilder sb = new StringBuilder("@page:object-searcher.jsp");
+        sb.append("?entityId=").append(getEntity().getId());
+        sb.append("&textField=").append(getTextField());
+        sb.append("&field=").append(field.getId());
+        if (getFilter() != null) {
+            sb.append("&filter=").append(getFilter().getId());
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public DescribedCriterion build(Entity entity, Field field, Map<String, String[]> parameters) {
+        final String[] values = parameters.get("value");
+        final List<Object> result = new ArrayList<>();
+        for (String value : values) {
+            result.add(getEntity().getDao().get(value));
+        }
+        return new DescribedCriterion(MessageFactory.info(DESCRIPTION_KEY, String.valueOf(result)), Restrictions.in(field.getProperty(), result));
+    }
+
+    public Entity getEntity() {
+        return entity;
+    }
+
+    public void setEntity(Entity entity) {
+        this.entity = entity;
+    }
+
+    public ListFilter getFilter() {
+        return filter;
+    }
+
+    public void setFilter(ListFilter filter) {
+        this.filter = filter;
+    }
+
+    public String getTextField() {
+        return textField;
+    }
+
+    public void setTextField(String textField) {
+        this.textField = textField;
+    }
+
+    public HttpServletRequest getRequest() {
+        return request;
+    }
+
+    public void setRequest(HttpServletRequest request) {
+        this.request = request;
+    }
+
+}
--- modules/jpm2-web/src/main/webapp/WEB-INF/jsp/searcher/object-searcher.jsp
+++ modules/jpm2-web/src/main/webapp/WEB-INF/jsp/searcher/object-searcher.jsp
@@ -1,20 +1,28 @@
 <%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
-<div id="${param.field}CollectionSearcherContainer">
-
+<div id="${param.field}objectSearcher" class="objectSearcher">
+    <label>
+        <input type="checkbox" id="${param.field}selectAll" /> <spring:message code="jpm.converter.collection.selectall" text="Select all" />
+    </label><br/><br/>
 </div>
 <script type="text/javascript">
     jpmLoad(function () {
+        $(document).on("click", "#${param.field}selectAll", function () {
+            $("input[name='value']").prop("checked", $(this).is(":checked"));
+        });
         $.ajax({
             url: "${cp}jpm/${param.entityId}.json",
             dataType: "json",
             async: false,
             data: {
                 textField: "${param.textField}",
-                filter: "${param.filter}"
+                filter: "${param.filter}",
+                ownerId: "${not empty owner?ownerId:''}"
             },
             success: function (data) {
+                var ids = "${param.value}".split(",");
                 $.each(data.results, function (i, item) {
-                    $("#${param.field}CollectionSearcherContainer").append("<label><input type='checkbox' value='" + item.id + "' name='value' />&nbsp;" + item.text + "</label><br/>");
+                    var checked = ($.inArray(item.id, ids) >= 0) ? 'checked' : '';
+                    $("#${param.field}objectSearcher").append("<label><input type='checkbox' " + checked + " value='" + item.id + "' name='value' />&nbsp;" + item.text + "</label><br/>");
                 });
             }
         });
