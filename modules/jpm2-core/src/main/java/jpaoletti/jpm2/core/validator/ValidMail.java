5cd9136e65f977b8e5eecf6156687b2c7269fe38
Jeronimo Jaoletti <jeronimo.paoletti@gmail.com>
2024-07-31 11:34:54 -0300

    E mail validation through regexp

--- modules/jpm2-core/src/main/java/jpaoletti/jpm2/core/validator/ValidMail.java
+++ modules/jpm2-core/src/main/java/jpaoletti/jpm2/core/validator/ValidMail.java
@@ -1,10 +1,10 @@
 package jpaoletti.jpm2.core.validator;
 
+import java.util.regex.Pattern;
 import jpaoletti.jpm2.core.message.Message;
 import jpaoletti.jpm2.core.message.MessageFactory;
 import jpaoletti.jpm2.core.model.FieldValidator;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.validator.routines.EmailValidator;
 
 /**
  *
@@ -13,12 +13,14 @@ import org.apache.commons.validator.routines.EmailValidator;
 public class ValidMail implements FieldValidator {
 
     private String message = "jpm.validator.mail.invalid";
+    private String regexPattern = "^[A-Za-z]+[a-zA-Z0-9._-]+[a-zA-Z0-9]@[A-Za-z]{2,}\\.[A-Za-z]{2,4}$";
 
     @Override
     public Message validate(Object object, Object convertedValue) {
         final String mail = convertedValue.toString();
         if (StringUtils.isNotEmpty(mail)) {
-            if (EmailValidator.getInstance().isValid(mail)) {
+            Pattern pattern = Pattern.compile(regexPattern);
+            if (pattern.matcher(mail).find()) {
                 return null;
             } else {
                 return MessageFactory.error(getMessage());
@@ -26,6 +28,14 @@ public class ValidMail implements FieldValidator {
         } else {
             return null;
         }
+    }
+
+    public String getRegexPattern() {
+        return regexPattern;
+    }
+
+    public void setRegexPattern(String regexPattern) {
+        this.regexPattern = regexPattern;
     }
 
     public String getMessage() {
