91f9445f88dc35870650d59a8fc6818dc3541049
Jeronimo Paoletti <jeronimo.paoletti@gmail.com>
2026-03-17 12:23:01 -0300

    ShowTag

---
+++ modules/jpm2-web-core/src/main/java/jpaoletti/jpm2/web/converter/ShowTag.java
@@ -0,0 +1,29 @@
+package jpaoletti.jpm2.web.converter;
+
+import jpaoletti.jpm2.core.converter.Converter;
+import jpaoletti.jpm2.core.exception.ConfigurationException;
+import jpaoletti.jpm2.core.exception.ConverterException;
+import jpaoletti.jpm2.core.model.ContextualEntity;
+import jpaoletti.jpm2.core.model.Field;
+import jpaoletti.jpm2.core.model.Tag;
+
+/**
+ * Converter to display a single Tag instance as a styled tag badge.
+ *
+ * @author jpaoletti
+ */
+public class ShowTag extends Converter {
+
+    @Override
+    public Object visualizeValue(ContextualEntity contextualEntity, Field field, Object instance, Object object, String instanceId) throws ConverterException, ConfigurationException {
+        final Tag value = (Tag) ((object == null) ? null : object);
+        if (value == null) {
+            return "";
+        }
+        final StringBuilder sb = new StringBuilder("<div class='show-tags-converter'>");
+        sb.append("<span data-id='").append(value.getId()).append("' style='").append(value.getStyle() != null ? value.getStyle() : "").append("' class='tag'>");
+        sb.append(value.getDescription());
+        sb.append("</span>");
+        return sb.append("</div>").toString();
+    }
+}
