426b659f7d2f0ec7194829da8cefc40978eae704
Jeronimo Paoletti <jeronimo.paoletti@gmail.com>
2026-03-26 13:48:14 -0300

    menu fix on mobile

--- modules/jpm2-web-bs5/src/main/webapp/static/js/jpm.js
+++ modules/jpm2-web-bs5/src/main/webapp/static/js/jpm.js
@@ -193,26 +193,9 @@ function initMenu() {
             // close all inner dropdowns when parent is closed
             document.querySelectorAll('.navbar .dropdown').forEach(function (everydropdown) {
                 everydropdown.addEventListener('hidden.bs.dropdown', function () {
-                    // after dropdown is hidden, then find all submenus
                     this.querySelectorAll('.submenu').forEach(function (everysubmenu) {
-                        // hide every submenu as well
                         everysubmenu.style.display = 'none';
                     });
-                });
-            });
-            document.querySelectorAll('.dropdown-menu a').forEach(function (element) {
-                element.addEventListener('click', function (e) {
-                    let nextEl = this.nextElementSibling;
-                    if (nextEl && nextEl.classList.contains('submenu')) {
-                        // prevent opening link if link needs to open dropdown
-                        e.preventDefault();
-                        console.log(nextEl);
-                        if (nextEl.style.display === 'block') {
-                            nextEl.style.display = 'none';
-                        } else {
-                            nextEl.style.display = 'block';
-                        }
-                    }
                 });
             });
         }
